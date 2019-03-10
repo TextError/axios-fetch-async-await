db01cc11be26fe603599db381e3115f421f17fdf
TextError <savinraul1983@yahoo.com>
2019-03-10 11:01:03 +0000

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Components
 import Home from './components/home/Home';
+import Axios from './components/axios/Axios';
+import Fetch from './components/fetch/Fetch';
+import AsyncAwait from './components/async-await/AsyncAwait';
+
+// Layout
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
+      <div className="app">
+        <Router>
+          <Switch>
+            <div className='container-fluid'>
+              <Navbar />
+              <Route exact path='/home' component={Home} />
+              <Route exact path='/axios' component={Axios} />
+              <Route exact path='/fetch' component={Fetch} />
+              <Route exact path='/async-await' component={AsyncAwait} />
+              <Footer />
+            </div>
+          </Switch>
+        </Router>
       </div>
     );
   }
