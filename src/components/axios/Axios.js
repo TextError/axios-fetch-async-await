d85aba3d59e1701f5f26adfb671ff243b9ce8788
TextError <savinraul1983@yahoo.com>
2019-03-11 09:41:29 +0000

    basic axios map

--- src/components/axios/Axios.js
+++ src/components/axios/Axios.js
@@ -3,8 +3,36 @@ import React, { Component } from 'react'
 export default class Axios extends Component {
   render() {
     return (
-      <div>
-        Axios
+      <div className='axios'>
+        <div className='row'>
+          <div className='col'>
+            <h5>Axios by Savin Raul-Calin</h5>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <div class="card">
+              <div class="card-body">
+                This is some text within a card body about axios.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <div class="card">
+              <div class="card-header">
+                <h5>GET Request</h5>
+              </div>
+              <div class="card-body">
+                <div>
+                  <button className='btn btn-primary' value='Get Todo' />
+                  <button className='btn btn-secondary' value='Clear' />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
